c0241fa6726cb41b0f8b4531dbd25d06dbe0d072
Murilo Prado de Lima <murilotempri@gmail.com>
2021-08-04 14:16:28 -0300

    Criado tela de listagen de eventos

--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+# Criando os campo e atribuindo as regras de cada campo
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    drecricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do evento')
+    data_criacao = models.DateTimeField(auto_now=True)
+    usuarios = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
